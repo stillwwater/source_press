ca99d187354a86188e35033ef21ac35666a1fdc8
stillwwater <stillwwater@gmail.com>
2017-05-23 11:26:31 -0300

    Added --silent option

--- lib/source_press.rb
+++ lib/source_press.rb
@@ -3,20 +3,30 @@ require_relative "source_press/press"
 require_relative "source_press/config"
 
 #
+# Runs compiler
+#
+def start_press(config_file, is_silent)
+  settings = Press::Settings.new(config_file)
+  Press::Compiler.new(settings, is_silent).run
+end
+
+#
 # Main entry point
 #
 def main(args)
-  config = ".press.yml"
+  is_silent = false
+  config    = ".press.yml"
 
   unless args.empty?
     abort(SourcePress::VERSION) if args[0] == "-v"
+    is_silent = args.include?("--silent")
 
     m = args[0].match(/config=(.*)/)
     config = m[1].strip unless m.nil?
 
     Config.generate(config) if args[0].strip == "gen-config"
   end
-
-  settings = Press::Settings.new(config)
-  Press::Compiler.new(settings).run
+  start_press(config, is_silent)
 end
+
+main(ARGV)
