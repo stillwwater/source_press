3fcf939b1c609e9c37814b63e82def18bc5bbfcd
stillwwater <stillwwater@gmail.com>
2017-05-24 12:40:57 -0300

    Main method now part of `SourcePress` class
    Avoids conflict with method names when using this gem as a library instead
    of a command-line utility.

--- lib/source_press.rb
+++ lib/source_press.rb
@@ -2,29 +2,31 @@ require_relative "source_press/version"
 require_relative "source_press/press"
 require_relative "source_press/config"
 
-#
-# Runs compiler
-#
-def start_press(config_file, is_silent)
-  settings = Press::Settings.new(config_file)
-  Press::Compiler.new(settings, is_silent).run
-end
-
-#
-# Main entry point
-#
-def main(args)
-  is_silent = false
-  config    = ".press.yml"
-
-  unless args.empty?
-    abort(SourcePress::VERSION) if args[0] == "-v"
-    is_silent = args.include?("--silent")
-
-    m = args[0].match(/config=(.*)/)
-    config = m[1].strip unless m.nil?
-
-    Config.generate(config) if args[0].strip == "gen-config"
+class SourcePress
+  #
+  # Runs compiler
+  #
+  def start_press(config_file, is_silent)
+    settings = Press::Settings.new(config_file)
+    Press::Compiler.new(settings, is_silent).run
   end
-  start_press(config, is_silent)
+
+  #
+  # Main entry point
+  #
+  def main(args = [])
+    is_silent = false
+    config    = ".press.yml"
+
+    unless args.empty?
+      abort(SourcePress::VERSION) if args[0] == "-v"
+      is_silent = args.include?("--silent")
+
+      m = args[0].match(/config=(.*)/)
+      config = m[1].strip unless m.nil?
+
+      Config.generate(config) if args[0].strip == "gen-config"
+    end
+    start_press(config, is_silent)
+  end
 end
